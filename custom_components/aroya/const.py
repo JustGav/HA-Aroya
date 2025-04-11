3fa807c0184a21ed091ea353e6019f00e7b0d240
JustGav <gavan@infiniteconnectivity.com>
2025-04-11 16:00:46 +0100

    Update const.py

--- custom_components/aroya/const.py
+++ custom_components/aroya/const.py
@@ -1,2 +1,6 @@
 DOMAIN = "aroya"
 API_BASE = "https://api.aroya.io/public_api"
+
+# Define units not exposed directly by Home Assistant
+UNIT_PARTS_PER_MILLION = "ppm"
+UNIT_MILLISEIMENS_PER_CM = "mS/cm"
